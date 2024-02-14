4377451c586027e112d5200847f9291a1672a680
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-02-14 22:19:24 +0200

    feat: add main page

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,18 @@
+
+const MainPage: React.FC = () => {
+  const onChooseLang = (e: React.MouseEvent<HTMLButtonElement>) => {
+    localStorage.setItem('locale', e.currentTarget.innerText)
+  }
+  return (
+    <>
+      <div>What is your preferred language?</div>
+      <div>Choose language</div>
+      <button onClick={onChooseLang}>English</button>
+      <button onClick={onChooseLang}>French</button>
+      <button onClick={onChooseLang}>German</button>
+      <button onClick={onChooseLang}>Spanish</button>
+    </>
+  )
+}
+
+export default MainPage;
