cbfca2c064331a8f050c32dfdac1e54511444876
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-02-17 18:51:06 +0200

    fix: code improvement

--- src/components/EmailPage/index.tsx
+++ src/components/EmailPage/index.tsx
@@ -7,7 +7,6 @@ import ButtonNext from "../Button";
 import ErrorMessage from "../ErrorMessage";
 import EmailField from "../EmailField";
 import PolicyMessage from "../PolicyMessage";
-import { Wrapper } from "./EmailPage.styled";
 import { PageContainerWithButton } from "../../styles/PageContainerWithButton";
 import { useTranslation } from "react-i18next";
 
@@ -33,18 +32,18 @@ const EmailPage:React.FC = () => {
 
   return (
     <PageContainerWithButton>
-      <>
+      <div>
         <Header />
         <TitleSection title={t("Email")} subtitle={t("Enter your email to get full access")} />
         <EmailField inputValue={inputValue} onChangeValue={onChangeValue} validate={validate} />
         {isError && inputValue !== '' ? <ErrorMessage text={t("Invalid email")}/> : <ErrorMessage text={""}/>}
         <PolicyMessage />
-      </>
-      <>
+      </div>
+      <div>
         <Link to="/finish">
           <ButtonNext onClick={onSubmitEmail} disabled={isError} title={t("Next")} />
         </Link>
-      </>
+      </div>
     </PageContainerWithButton>
   )
 };
